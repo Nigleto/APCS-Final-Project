a6333fae9190bfed661613b1678f8645c10ef6e6
NS88607 <99279923+NS88607@users.noreply.github.com>
2022-05-25 08:53:12 -0700

    Jpanel Changed

--- Example.java
+++ Example.java
@@ -1,11 +1,11 @@
 import java.awt.*;
 import javax.swing.*;
-public class Jpanel{
-Jpanel(){
-JFrame a = new JFrame("Stock Interface");
+public class Example{
+Example(){
+JFrame a = new JFrame("example");
 JPanel p = new JPanel();
 p.setBounds(40,70,200,200);
-JButton b = new JButton("Choice 1");
+JButton b = new JButton("click me");
 b.setBounds(60,50,80,40);
 p.add(b);
 a.add(p);
@@ -15,6 +15,6 @@ a.setVisible(true);
 }
 public static void main(String args[])
 {
-new Jpanel();
+new Example();
 }
 }
