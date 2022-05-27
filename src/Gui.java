d76d05076d220c55157ea60ce875051eb545ab3a
Nigel P <58761175+Nigleto@users.noreply.github.com>
2022-05-27 09:29:22 -0700

    add

--- src/Gui.java
+++ src/Gui.java
@@ -3,6 +3,7 @@ package src;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.URL;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -37,6 +38,7 @@ public class Gui implements ActionListener {
     JMenuItem LogOut;
     JLabel stockListLabel;
     JComboBox watchList; 
+    JTextField stockOption;
 
     Gui() {
         jframe = new JFrame("Welcome!");
@@ -172,6 +174,71 @@ public class Gui implements ActionListener {
         }
         if (e.getSource().equals(Options)) {
             if(Options.getSelectedIndex()==0){
+                String options = new String("0 - price, 1 - volAvg, 2 - mktCap, 3 - range, 4 - ceo.  ");
+                String options1 = new String("OR 5 - add stock to your Watchlist, 6 - add stock to your Portfolio.");
+                boolean x = false;
+
+                jframe.getContentPane().removeAll();
+                System.out.println("Check Stock");
+                URL url = new URL("https://financialmodelingprep.com/api/v3/profile/" + ticker
+                    + "?apikey=9e32e1c117e9206264ef7c63453dca84");
+
+            System.out.println("What would you like to do with this stock? ");
+            System.out.println("The options are: " + options + options1);
+            String keyInput = scan.nextLine();
+
+            while(x){
+                switch(keyInput){
+                    case "0":
+                        StockEvent stockWithKey = new StockEvent("price", url);
+                        System.out.println(stockWithKey.getValue());
+                        x = false;
+                        break;
+                    case "1":
+                        StockEvent stockWithKey1 = new StockEvent("volAvg", url);
+                        System.out.println(stockWithKey1.getValue());
+                        x = false;
+                        break;
+                    case "2":
+                        StockEvent stockWithKey2 = new StockEvent("mktCap", url);
+                        System.out.println(stockWithKey2.getValue());
+                        x = false;
+                        break;
+                    case "3":
+                        StockEvent stockWithKey3 = new StockEvent("range", url);
+                        System.out.println(stockWithKey3.getValue());
+                        x = false;
+                        break;
+                    case "4":
+                        StockEvent stockWithKey4 = new StockEvent("ceo", url);
+                        System.out.println(stockWithKey4.getValue());
+                        x = false;
+                        break;
+                    case "5":
+                        Stock stock = new Stock(ticker, url);
+                        user.addWatchlistStock(stock);
+                        System.out.println("Done");
+                        System.out.println(user.getWatchlist().get(0));
+                        x = false;
+                        break;
+                    case "6":
+                        Stock stock1 = new Stock(ticker, url);
+                        user.addWatchlistStock(stock1);
+                        System.out.println("Done");
+                        x = false;
+                        break;
+                    default:
+                        System.out.println("Please enter a valid option. ");
+                }
+            }
+        }
+
+        catch (Exception e) {
+            start();
+
+        }
+
+
             }
             if (Options.getSelectedIndex() == 1) {
                 jframe.getContentPane().removeAll();
