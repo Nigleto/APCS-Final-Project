029ecc8b5b7b0a2d0b514e0befdb691ac90a3ff3
NS88607 <99279923+NS88607@users.noreply.github.com>
2022-05-26 22:54:50 -0700

    Update Gui.java

--- src/Gui.java
+++ src/Gui.java
@@ -1,12 +1,15 @@
 package src;
 
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 public class Gui implements ActionListener {
     JFrame jframe; 
@@ -17,19 +20,23 @@ public class Gui implements ActionListener {
     JTextField Email;
     JLabel askPW;
     JTextField Password; 
-    JButton Submit; 
+    JButton Login; 
+    JButton CreateAccount;
     static User user;
     static Database db = new Database();
+    JLabel AccountCreationMsg;
+    JButton Back; 
+    JComboBox Options;
 
     Gui() {
-        jframe = new JFrame("Stock Login");
+        jframe = new JFrame("Welcome!");
         Yes = new JButton("Yes");
         No = new JButton("No");
         Yes.setBounds(30,50,80,40);
         No.setBounds(150,50,80,40);
         Yes.addActionListener(this);
         No.addActionListener(this);
-        jframe.setSize(300, 200 );
+        jframe.setSize(270, 150 );
         jframe.setLayout(null);
         jframe.setVisible(true);
         question = new JLabel("Do you have an account with us?");
@@ -45,6 +52,7 @@ public class Gui implements ActionListener {
 public void actionPerformed(ActionEvent e) {
     if(e.getSource()== Yes){
         jframe.getContentPane().removeAll();
+        jframe.setTitle("Log In");
         jframe.setSize(500, 200);
         askEmail= new JLabel("Email:");
         askEmail.setBounds(30, 20, 200, 20);
@@ -54,23 +62,85 @@ public void actionPerformed(ActionEvent e) {
         askPW.setBounds(30, 60, 200, 20);
         Password = new JTextField(20);
         Password.setBounds(90, 60, 200, 20);
-        Submit = new JButton("Submit"); 
-        Submit.setBounds(375, 130, 90, 20);
-        Submit.addActionListener(this);
+        Login = new JButton("Log In"); 
+        Login.setBounds(375, 130, 90, 20);
+        Login.addActionListener(this);
         jframe.add(askPW); 
         jframe.add(askEmail);
         jframe.add(Email);
         jframe.add(Password); 
-        jframe.add(Submit); 
+        jframe.add(Login); 
         jframe.repaint();
     }
     if(e.getSource()==No){
+        jframe.getContentPane().removeAll();
+        jframe.setSize(500, 200);
+        jframe.setTitle("Create an Account");
+        askEmail= new JLabel("Email:");
+        askEmail.setBounds(30, 20, 200, 20);
+        Email= new JTextField(20);
+        Email.setBounds(70,20,200,20);
+        askPW= new JLabel("Password:");
+        askPW.setBounds(30, 60, 200, 20);
+        Password = new JTextField(20);
+        Password.setBounds(90, 60, 200, 20);
+        CreateAccount = new JButton("Create Account"); 
+        CreateAccount.setBounds(330 , 130, 140, 20);
+        CreateAccount.addActionListener(this);
+        jframe.add(askPW); 
+        jframe.add(askEmail);
+        jframe.add(Email);
+        jframe.add(Password); 
+        jframe.add(CreateAccount); 
+        jframe.repaint();
     }
-    if(e.getSource()==Submit){
-        user = new User(Email.getText(), Password.getText());
-        db.getUserlist().add(user);
-        
-
+    if(e.getSource()==CreateAccount){
+        //need to add check for duplicate accounts
+        //user = new User(Email.getText(), Password.getText());
+       // db.getUserlist().add(user);
+        jframe.getContentPane().removeAll();
+        jframe.setSize(300, 200 );
+        AccountCreationMsg = new JLabel("Account Successfully Created!");
+        AccountCreationMsg.setBounds(59, 15, 200, 100);
+        Back= new JButton("To Login");
+        Back.setFont(new Font("",Font.TRUETYPE_FONT, 10));
+        Back.setBounds(100,90,80,20); 
+        Back.addActionListener(this);
+        jframe.add(AccountCreationMsg);
+        jframe.add(Back); 
+        jframe.repaint();
+    }
+    if(e.getSource()==Login){
+        //Check to see if login is valid
+        jframe.getContentPane().removeAll();
+        jframe.setTitle("Your Account");
+        String optionArr[]= {"Price", "Average Volume", "Market Cap", "Range", "CEO", "Add stock to watchlist","Add stock to Portfolio" };
+        Options = new JComboBox(optionArr);
+        Options.setBounds(40, 40, 150, 20);
+        jframe.add(Options);
+        jframe.repaint();
+    }
+    if(e.getSource()==Back){
+        jframe.getContentPane().removeAll();
+        jframe.setTitle("Log In");
+        jframe.setSize(500, 200);
+        askEmail= new JLabel("Email:");
+        askEmail.setBounds(30, 20, 200, 20);
+        Email= new JTextField(20);
+        Email.setBounds(70,20,200,20);
+        askPW= new JLabel("Password:");
+        askPW.setBounds(30, 60, 200, 20);
+        Password = new JTextField(20);
+        Password.setBounds(90, 60, 200, 20);
+        Login = new JButton("Log In"); 
+        Login.setBounds(375, 130, 90, 20);
+        Login.addActionListener(this);
+        jframe.add(askPW); 
+        jframe.add(askEmail);
+        jframe.add(Email);
+        jframe.add(Password); 
+        jframe.add(Login); 
+        jframe.repaint();
     }
     
 }
